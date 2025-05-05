f79c2744c8650aa107a22bbbc8532ae3608b8740
harakki <124341753+harakki@users.noreply.github.com>
2025-05-05 20:20:29 +0700

    Complete general form solution generation

--- main.py
+++ main.py
@@ -213,17 +213,20 @@ def get_optimal_solution(df: pd.DataFrame):
     all_variables = headers[1:-1]
     m = len(df) - 1  # Количество ограничений
 
+    basis_map = {}
     solution = {var: Fraction(0) for var in all_variables}
 
-    basis = []
     for eq in range(m):
         b = df.iloc[eq]["Базис"]
-        basis.append(b)
         if b in all_variables:
+            basis_map[eq] = b
             solution[b] = Fraction(str(df.iloc[eq]["Св. член"]))
 
     optimal_z = Fraction(str(df.iloc[m]["Св. член"]))
-    return solution, optimal_z, all_variables, basis
+    basis = list(basis_map.values())
+    non_basis = sorted([v for v in all_variables if v not in basis])
+
+    return optimal_z, solution, all_variables, basis, non_basis
 
 
 def print_optimal_solution(optimal_z: Fraction, solution: dict):
@@ -233,8 +236,60 @@ def print_optimal_solution(optimal_z: Fraction, solution: dict):
     console.print(f"Z = ({vars_str}) = ({values_str}) = {optimal_z}")
 
 
-def get_general_form_solution(df: pd.DataFrame):
-    ...
+def get_general_form_solution(df: pd.DataFrame, basis: list, non_basis: list):
+    m = len(df) - 1  # Количество ограничений
+    general_solution = {}
+
+    # Выражения для базисных переменных
+    basis_row_index = {df.iloc[i]["Базис"]: i for i in range(m) if df.iloc[i]["Базис"] in basis}
+    for b, row_index in basis_row_index.items():
+        general_solution[b] = build_general_form_solution_rhs_expr(df, row_index, non_basis)
+
+    # Выражения для небазисных переменных
+    for v in non_basis:
+        general_solution[v] = f"{v}"
+
+    # 3. Выражение для Z
+    z_expr = build_general_form_solution_rhs_expr(df, m, non_basis)
+
+    return general_solution, z_expr
+
+
+def build_general_form_solution_rhs_expr(df: pd.DataFrame, row_index: int, non_basis: list):
+    expr_parts = [str(df.iloc[row_index]["Св. член"])]
+    for v in non_basis:
+        coeff = df.iloc[row_index][v]
+        term = format_term(-coeff, v)
+        if term:
+            expr_parts.append(term)
+
+    return sp.sympify("".join(expr_parts))
+
+
+def format_term(coeff: Fraction, var: str):
+    if coeff == 0:
+        return ""
+
+    sign = "+" if coeff > 0 else "-"
+    abs_coeff = abs(coeff)
+
+    if abs_coeff == 1:
+        term = f"{sign} {var}"
+    else:
+        term = f"{sign} {abs_coeff} * {var}"
+    return term
+
+
+def print_general_form_solution(solution_expr_dict: dict, z_expr: str, non_basis: list):
+    console = Console(force_terminal=True)
+    print("\nОбщий вид решения:")
+    for var in solution_expr_dict.keys():
+        console.print(f"{var} = {solution_expr_dict[var]}")
+
+    console.print(f"Z = {z_expr}")
+
+    if non_basis:
+        console.print(", ".join([f"{v} >= 0" for v in non_basis]))
 
 
 def main():
@@ -243,12 +298,16 @@ def main():
     print("Исходная проблема:")
     print_problem(z_expr, goal, constraints)
     z_expr, canonized_constraints, artificial_variables = canonize_problem(z_expr, goal, constraints)
+
     df = build_simplex_table(z_expr, canonized_constraints, variables, artificial_variables)
     df = dual_simplex_method(df)
     print_simplex_table(df)
-    opt_solution, opt_z, _, _ = get_optimal_solution(df)
-    print_optimal_solution(opt_z, opt_solution)
-    get_general_form_solution(df)
+
+    optimal_z, solution, all_variables, basis, non_basis = get_optimal_solution(df)
+    print_optimal_solution(optimal_z, solution)
+
+    general_solution, z_expression = get_general_form_solution(df, basis, non_basis)
+    print_general_form_solution(general_solution, z_expression, non_basis)
 
 
 if __name__ == "__main__":
